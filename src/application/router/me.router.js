a318995ecc0631242728e639da1b727041c4ba6f
LeQuocPhong2k2 <phonglehn2002@gmai.com>
2025-04-06 21:15:35 +0700

    feat:update router getMe

--- src/application/router/me.router.js
+++ src/application/router/me.router.js
@@ -2,7 +2,8 @@ import express from "express";
 import ROUTING from "../../constants/Routes.js";
 import userController from "../handlers/user.controller.js";
 const router = express.Router();
+import { authenticateToken } from "../middleware/authentication.middleware.js";
 
-router.get(ROUTING.INDEX, userController.getMe);
+router.get(ROUTING.ME, authenticateToken, userController.getMe);
 
 export default router
