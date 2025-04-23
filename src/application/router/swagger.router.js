b3e17813e8831f0d3f3eb694448a60025b7afc51
Quindart <quang.le@aegona.com>
2025-04-23 13:33:18 +0700

    fix: config

--- src/application/router/swagger.router.js
+++ src/application/router/swagger.router.js
@@ -1,9 +1,6 @@
 import express from "express";
-import ROUTING from "../../constants/Routes.js";
-import swaggerUi from "swagger-ui-express";
 const router = express.Router();
 
-// router.use(ROUTING.SWAGGER, swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default router;
     
