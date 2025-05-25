1fb0f2ebfe49e8d7d0bea7bc478a6e1e06400c22
HuyVu2379 <21021491.huy@student.iuh.edu.vn>
2025-05-26 01:31:25 +0700

    feat: add login function with google

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,8 +1,6 @@
-// config/firebase.js
 import admin from 'firebase-admin';
 import serviceAccount from './serviceAccountKey.json' assert { type: 'json' };
 
-// Chỉ khởi tạo Firebase Admin SDK nếu chưa được khởi tạo
 if (!admin.apps.length) {
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
